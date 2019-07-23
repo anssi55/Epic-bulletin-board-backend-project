71fef48816a3d6f8f816c9c272cb911fa79e69a2
Anssi Laurento <anssi.laurento@gmail.com>
2019-07-23 15:42:44 +0300

    trying database connection 2

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@ module.exports = {
   type: 'mysql',
   host: process.env.JAWSDB_HOST,
   port: 3306,
-  username: process.envJAWSDB_USERNAME,
-  password: process.envJAWSDB_PASSWORD,
-  database: process.envJAWSDB_DATABASE
+  username: process.env.JAWSDB_USERNAME,
+  password: process.env.JAWSDB_PASSWORD,
+  database: process.env.JAWSDB_DATABASE
 };
