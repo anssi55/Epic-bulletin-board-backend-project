87830ff0116bae62058d91411f121377bdd2d6c9
Anssi Laurento <anssi.laurento@gmail.com>
2019-07-24 18:15:53 +0300

    congired ormconfig to use more env variables, removed unused packages and fixed circle config

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,11 @@
 module.exports = {
   type: 'mysql',
   host: process.env.DB_HOST,
-  port: 3306,
+  port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  synchronize: true,
+  synchronize: process.env.DB_SYNC,
   logging: false,
   entities: ['build/orm/entities/*.js'],
   migrations: ['build/orm/migration/**/*.js'],
