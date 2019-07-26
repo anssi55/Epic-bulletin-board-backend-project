228db9b2fdfae70736de6945f43d1b19855957a8
Anssi Laurento <anssi.laurento@gmail.com>
2019-07-26 16:12:25 +0300

    modified error.middleware to use boom

--- app/middleware/error.middleware.ts
+++ app/middleware/error.middleware.ts
@@ -1,18 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
-import HttpException from '../exceptions/HttpException';
-
-function errorMiddleware(
-  error: HttpException,
-  request: Request,
-  response: Response,
-  next: NextFunction
-) {
-  const status = error.status || 500;
-  const message = error.message || 'Something went wrong';
-  response.status(status).send({
-    status,
-    message
-  });
+import Boom from 'boom';
+function errorMiddleware(error: Error, req: Request, res: Response, next: NextFunction) {
+  if (error.name === 'SyntaxError') {
+    res.status(400).send(Boom.badRequest().output.payload);
+  } else {
+    Boom.badRequest;
+    const boom = Boom.boomify(error).output;
+    res.status(boom.statusCode).send(boom.payload);
+  }
 }
 
 export default errorMiddleware;
