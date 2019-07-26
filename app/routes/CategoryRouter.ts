e86b55ede1d03f5355ae2928eb3850d18e29b59f
Anssi Laurento <anssi.laurento@gmail.com>
2019-07-26 16:16:14 +0300

    finished category/postrouter with boom errors

--- app/routes/CategoryRouter.ts
+++ app/routes/CategoryRouter.ts
@@ -20,7 +20,7 @@ class CategoriesRouter {
       res.status(boom.statusCode).send(boom.payload);
     }
   };
-  //Get one category spesified by id
+
   public getOne = async (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id;
     try {
@@ -35,27 +35,53 @@ class CategoriesRouter {
       res.status(boom.statusCode).send(boom.payload);
     }
   };
-  //Create one new category to database
+
   public create = async (req: Request, res: Response, next: NextFunction) => {
     let category = new Category();
     category.name = req.body.name;
     category.description = req.body.description;
     try {
-      await this.categoryRepo.save(category);
-
-      res.status(200).send(category);
+      const result = await this.categoryRepo.save(category);
+      res.status(200).send(result);
     } catch (error) {
       const boom = Boom.boomify(error).output;
       res.status(boom.statusCode).send(boom.payload);
     }
   };
-  //Update one category in database
-  public update(req: Request, res: Response, next: NextFunction) {
-    res.send('Nothing');
-  }
-  public delete(req: Request, res: Response, next: NextFunction) {
-    res.send('Nothing');
-  }
+
+  public update = async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    try {
+      let category = await this.categoryRepo.findOne(id);
+      if (category) {
+        category.description = req.body.description;
+        category.name = req.body.name;
+        const result = await this.categoryRepo.save(category);
+        res.status(200).send(result);
+      } else {
+        res.status(404).send(Boom.notFound('Category not found').output.payload);
+      }
+    } catch (error) {
+      const boom = Boom.boomify(error).output;
+      res.status(boom.statusCode).send(boom.payload);
+    }
+  };
+
+  public delete = async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    try {
+      let category = await this.categoryRepo.findOne(id);
+      if (category) {
+        const result = await this.categoryRepo.delete(category);
+        res.status(200).send('Deleted category: ' + JSON.stringify(category));
+      } else {
+        res.status(404).send(Boom.notFound('Category not found').output.payload);
+      }
+    } catch (error) {
+      const boom = Boom.boomify(error).output;
+      res.status(boom.statusCode).send(boom.payload);
+    }
+  };
 }
 
 export default CategoriesRouter;
--- app/routes/PostRouter.ts
+++ app/routes/PostRouter.ts
@@ -19,7 +19,7 @@ class PostRouter {
       if (posts.length > 0) {
         res.status(200).send(posts);
       } else {
-        res.status(404).send(Boom.notFound('Posts not found'));
+        res.status(404).send(Boom.notFound('Posts not found').output.payload);
       }
     } catch (error) {
       const boom = Boom.boomify(error).output;
@@ -47,11 +47,17 @@ class PostRouter {
     post.topic = req.body.topic;
     post.post = req.body.post;
     post.datetime = new Date(Date.now());
-    post.category = req.body.categoryId;
+    let categoryId = req.body.categoryId;
     post.pinned = req.body.pinned;
     try {
-      await this.postRepo.save(post);
-      res.status(200).send(post);
+      const category = await this.categoryRepo.findOne(categoryId);
+      if (category) {
+        post.category = category;
+        const result = await this.postRepo.save(post);
+        res.status(200).send(result);
+      } else {
+        res.status(404).send(Boom.notFound('Category not found').output.payload);
+      }
     } catch (error) {
       const boom = Boom.boomify(error);
       res.status(boom.output.statusCode).send(boom.output.payload);
@@ -65,16 +71,20 @@ class PostRouter {
     try {
       const post = await this.postRepo.findOne(id);
       const category = await this.categoryRepo.findOne(categoryId);
-      if (post !== undefined) {
+      if (post) {
         post.topic = req.body.topic;
         post.post = req.body.post;
         post.modified = new Date(Date.now());
         post.pinned = req.body.pinned;
-        if (category !== undefined) {
+        if (category) {
           post.category = category;
+        } else {
+          res.status(404).send(Boom.notFound('Category not found').output.payload);
         }
-        await this.postRepo.save(post);
-        res.status(200).send(post);
+        const result = await this.postRepo.save(post);
+        res.status(200).send(result);
+      } else {
+        res.status(404).send(Boom.notFound('Post not found').output.payload);
       }
     } catch (error) {
       const boom = Boom.boomify(error);
@@ -85,7 +95,13 @@ class PostRouter {
   public delete = async (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id;
     try {
-      const post = await this.postRepo.delete(id);
+      const post = await this.postRepo.findOne(id);
+      if (post) {
+        await this.postRepo.delete(post);
+        res.status(200).send('Deleted post:' + JSON.stringify(post));
+      } else {
+        res.status(404).send('Post not found');
+      }
       res.status(200).send(post);
     } catch (error) {
       const boom = Boom.boomify(error);
