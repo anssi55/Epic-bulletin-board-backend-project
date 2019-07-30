68d9a010c4f2207e2b20e402ba7302bebab9524c
Anssi Laurento <anssi.laurento@gmail.com>
2019-07-30 15:41:41 +0300

    fixed envValidator to work with heroku's env variables

--- app/envValidator.ts
+++ app/envValidator.ts
@@ -4,23 +4,21 @@ import { validate } from 'class-validator';
 import dotenv from 'dotenv';
 
 function validateEnvVariables() {
-  let result2;
-  const result = dotenv.config();
-  if (result.error === undefined) {
-    result2 = result.parsed;
-  } else {
-    result2 = process.env;
-  }
-  const variables = plainToClass(EnvVariables, result2);
-  return validate(variables).then(errors => {
+  const variablesToProcess = getEnvVariables();
+  const envVariables = plainToClass(EnvVariables, variablesToProcess);
+  return validate(envVariables).then(errors => {
     if (errors.length > 0) {
       const message =
         'Validation error(s): ' + errors.map(error => Object.values(error.constraints)).join(', ');
       throw message;
     } else {
-      return variables;
+      return envVariables;
     }
   });
+}
+function getEnvVariables() {
+  const variablesFromFile = dotenv.config();
+  return !variablesFromFile.error ? variablesFromFile.parsed : process.env;
 }
 
 export default validateEnvVariables;
