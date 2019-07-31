3c779778e3250322a71549ef3443e1d14e440f96
Anssi Laurento <anssi.laurento@gmail.com>
2019-07-31 14:55:40 +0300

    made models and transfered some functionality out of controllers

---
+++ app/models/CategoryModel.ts
@@ -0,0 +1,88 @@
+import { Repository } from 'typeorm';
+import { Dependencies } from '../Types';
+import Category from '../orm/entities/Category';
+import Boom from 'boom';
+
+class CategoryModel {
+  private categoryRepo: Repository<Category>;
+
+  constructor(opts: Dependencies) {
+    this.categoryRepo = opts.categoryRepo;
+  }
+
+  public getAllCategories = async () => {
+    try {
+      const categories = await this.categoryRepo.find();
+      if (categories.length > 0) {
+        return categories;
+      } else {
+        throw Boom.notFound('Categories not found');
+      }
+    } catch (error) {
+      throw Boom.isBoom(error)
+        ? error
+        : Boom.serverUnavailable('Something wrong with the database connection');
+    }
+  };
+
+  public getOneCategory = async (categoryId: number) => {
+    try {
+      const category = await this.categoryRepo.findOne(categoryId);
+      if (category) {
+        return category;
+      } else {
+        throw Boom.notFound('Category not found');
+      }
+    } catch (error) {
+      throw Boom.isBoom(error)
+        ? error
+        : Boom.serverUnavailable('Something wrong with the database connection');
+    }
+  };
+
+  public createCategory = async (newCategory: Category) => {
+    try {
+      const result = await this.categoryRepo.save(newCategory);
+      return result;
+    } catch (error) {
+      throw Boom.isBoom(error)
+        ? error
+        : Boom.serverUnavailable('Something wrong with the database connection');
+    }
+  };
+
+  public modifyCategory = async (modifiedCategory: Category) => {
+    try {
+      let originalCategory = await this.categoryRepo.findOne(modifiedCategory.id);
+      if (originalCategory) {
+        originalCategory.name = originalCategory.name;
+        originalCategory.description = originalCategory.description;
+        const result = await this.categoryRepo.save(originalCategory);
+        return result;
+      } else {
+        throw Boom.notFound('Category not found');
+      }
+    } catch (error) {
+      throw Boom.isBoom(error)
+        ? error
+        : Boom.serverUnavailable('Something wrong with the database connection');
+    }
+  };
+
+  public deleteCategory = async (categoryId: number) => {
+    try {
+      const categoryToDelete = await this.categoryRepo.findOne(categoryId);
+      if (categoryToDelete) {
+        const result = await this.categoryRepo.save(categoryToDelete);
+        return result;
+      } else {
+        throw Boom.notFound('Category not found');
+      }
+    } catch (error) {
+      throw Boom.isBoom(error)
+        ? error
+        : Boom.serverUnavailable('Something wrong with the database connection');
+    }
+  };
+}
+export default CategoryModel;
--- app/models/PostModel.ts
+++ app/models/PostModel.ts
@@ -7,6 +7,7 @@ import Boom from 'boom';
 class PostModel {
   private postRepo: Repository<Post>;
   private categoryRepo: Repository<Category>;
+
   constructor(opts: Dependencies) {
     this.postRepo = opts.postRepo;
     this.categoryRepo = opts.categoryRepo;
@@ -18,12 +19,15 @@ class PostModel {
       if (posts.length > 0) {
         return posts;
       } else {
-        throw Boom.notFound('Post not found');
+        throw Boom.notFound('Posts not found');
       }
-    } catch {
-      return Boom.serverUnavailable('Something wrong with the database connection');
+    } catch (error) {
+      throw Boom.isBoom(error)
+        ? error
+        : Boom.serverUnavailable('Something wrong with the database connection');
     }
   };
+
   public getOnePost = async (postId: number) => {
     try {
       const post = await this.postRepo.findOne(postId);
@@ -32,24 +36,71 @@ class PostModel {
       } else {
         throw Boom.notFound('Post not found');
       }
-    } catch {
-      return Boom.serverUnavailable('Something wrong with the database connection');
+    } catch (error) {
+      throw Boom.isBoom(error)
+        ? error
+        : Boom.serverUnavailable('Something wrong with the database connection');
     }
   };
-  public savePost = async (post: Post, categoryId: number) => {
-    post.datetime = new Date(Date.now());
+
+  public createPost = async (newPost: Post, categoryId: number) => {
+    newPost.datetime = new Date(Date.now());
     try {
       const category = await this.categoryRepo.findOne(categoryId);
       if (category) {
-        post.category = category;
+        newPost.category = category;
+        const result = await this.postRepo.save(newPost);
+        return result;
+      } else {
+        throw Boom.notFound('Category not found');
+      }
+    } catch (error) {
+      throw Boom.isBoom(error)
+        ? error
+        : Boom.serverUnavailable('Something wrong with the database connection');
+    }
+  };
+
+  public modifyPost = async (modifiedPost: Post, categoryId: number) => {
+    try {
+      let originalPost = await this.postRepo.findOne(modifiedPost.id);
+      const originalCategory = await this.categoryRepo.findOne(categoryId);
+      if (originalPost) {
+        originalPost.topic = modifiedPost.topic;
+        originalPost.post = modifiedPost.post;
+        originalPost.modified = new Date(Date.now());
+        originalPost.pinned = modifiedPost.pinned;
+        if (originalCategory) {
+          originalPost.category = originalCategory;
+        } else {
+          throw Boom.notFound('Category not found');
+        }
+        const result = await this.postRepo.save(originalPost);
+        return result;
+      } else {
+        throw Boom.notFound('Post not found');
+      }
+    } catch (error) {
+      throw Boom.isBoom(error)
+        ? error
+        : Boom.serverUnavailable('Something wrong with the database connection');
+    }
+  };
+
+  public deletePost = async (postId: number) => {
+    try {
+      const post = await this.postRepo.findOne(postId);
+      if (post) {
         const result = await this.postRepo.save(post);
         return result;
       } else {
-        return Boom.notFound('Category not found');
+        throw Boom.notFound('Post not found');
       }
-    } catch {
-      return Boom.serverUnavailable('Something wrong with the database connection');
+    } catch (error) {
+      throw Boom.isBoom(error)
+        ? error
+        : Boom.serverUnavailable('Something wrong with the database connection');
     }
   };
-  public modifyPost = async (post: Post) => {};
 }
+export default PostModel;
